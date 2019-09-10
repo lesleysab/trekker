9c63b3365a8a944e2b62664f9b0689a9be358553
Lesley Sablatura <lesleysab210@gmail.com>
2019-09-09 20:48:22 -0500

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import '../css/home.css'
+
+
+class Home extends React.Component {
+    render() {
+  
+        return (
+            <Fragment>
+<div className="home-page-wrapper">
+    <div className ="hero-wrapper">
+
+        
+        </div>
+
+
+</div>
+
+            </Fragment>
+        );
+    }
+}
+
+export default Home;
